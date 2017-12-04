d4deda2217530d8bddaf6e353bd6ade62ec3021a
Feynman Liang <feynman.liang@gmail.com>
2017-12-03 18:50:13 -0800

    Logistic regression plot

--- logreg.py
+++ logreg.py
@@ -9,8 +9,19 @@ from time import time
 
 from fjlt import fjlt_usp
 
+# https://sachinashanbhag.blogspot.com/2014/05/numerically-approximation-of-log-1-expy.html
+@np.vectorize
+def safe_log_one_plus_exp(y):
+    if y > 35:
+        return y
+    elif y < -10:
+        return np.exp(y)
+    else:
+        return np.log(1 + np.exp(y))
+
 def logddpsi(u):
-    return u - 2*np.log(np.exp(u) + 1)
+    return u - 2*safe_log_one_plus_exp(u)
+    #return u - 2*np.log(1 + np.exp(u))
 
 def sketched_hess_sqrt(x, m, A):
     hess_sqrt = A.transpose() * np.exp(0.5 * logddpsi(A.dot(x)))
@@ -21,12 +32,16 @@ def sketched_hess_sqrt(x, m, A):
 def ihs(fun, x0, jac, m, A, args=(), callback=None, **options):
     x = x0
     nit = 0
+    prev_fun = float("Inf")
+    stepped_fun = float("Inf")
     for _ in range(25):
         nit += 1
         SH = sketched_hess_sqrt(x, m, A)
         try:
             step = linalg.solve(SH.transpose().dot(SH), jac(x))
-            if (fun(x - step) < fun(x)):
+            stepped_fun = fun(x - step)
+            if (stepped_fun < prev_fun):
+                prev_fun = stepped_fun
                 x = x - step
         except:
             continue
@@ -43,12 +58,11 @@ if __name__ == "__main__":
     random.seed(42)
 
     d = 100
+    n = 2 ** 15 # 16 => 65536
     m = 4*d # for ROS IHS
-    n = 65536
 
     plt.rc('text', usetex=True)
     fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(12, 8))
-
 
     for row, rho in enumerate([0.0, 0.7, 0.9]):
         # generate problem
@@ -56,6 +70,7 @@ if __name__ == "__main__":
                 np.zeros(d),
                 rho*np.ones((d, d)) + np.diag(np.ones(d) - rho),
                 size=n)
+        A = A / random.chisquare(100, size=A.shape) # make student-t
         x_opt = random.normal(0, 1, size=d)
         x_opt = x_opt / linalg.norm(x_opt)
         y = np.ones(n)
@@ -66,8 +81,7 @@ if __name__ == "__main__":
         jac = grad(objective)
         hess = jacobian(jac)
 
-        methods = ["BFGS", "TNC", "trust-ncg", "IHS"]
-        methods = ["IHS"]
+        methods = ["BFGS", "TNC", "trust-exact", "IHS"]
         markers = ['^', 'o', 'D', 'x']
         for i,method in enumerate(methods):
             print(rho, method)
